aa72dc8b4a92b6193b9049ea5690be2014d9743a
yang haotse <ha91252@gmail.com>
2023-10-31 13:11:19 +0800

    add favicon

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
+app.use(express.static('public'))
+app.use(express.json())
 
 app.use(routes)
 
