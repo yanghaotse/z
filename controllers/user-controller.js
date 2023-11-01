f186940c039c4be0169690722c497942de06b1c5
yang haotse <ha91252@gmail.com>
2023-11-01 15:27:45 +0800

    feat: add signup

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,19 @@
 const bcrypt = require('bcryptjs')
+const { User } = require('../models')
 
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
+  },
+  signUp: async(req, res) => {
+    const { account, name, email, password, checkPassword } = req.body
+    const hashPassword = await bcrypt.hash(password, 10)
+    const user = await User.create({
+      account,
+      name,
+      password: hashPassword
+    })
+    res.render('signin')
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const admin = require('./modules/admin')
 router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/tweets', tweetController.getTweets)
 router.use('/', (req, res) => res.redirect('/tweets'))
 
