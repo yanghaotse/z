36d9796be0c82192fac1b076b2a8b2a7079f8092
yang haotse <ha91252@gmail.com>
2023-11-06 12:21:21 +0800

    feat: add admin signin page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,9 @@
 const adminController = {
   getTweets: (req, res) => {
     res.render('admin/tweets')
+  },
+  signInPage: (req, res, next) => {
+    res.render('admin/signin')
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
+router.get('/signin', adminController.signInPage)
 router.get('/tweets', adminController.getTweets)
 router.use('/', (req, res) => res.redirect('/admin/tweets'))
 
