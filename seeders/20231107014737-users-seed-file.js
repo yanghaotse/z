2f5542333f74a09c5c38a5f5a84f139acd4b2843
yang haotse <ha91252@gmail.com>
2023-11-07 11:06:42 +0800

    feat: add users seeder

---
+++ seeders/20231107014737-users-seed-file.js
@@ -0,0 +1,41 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+
+const Users = [{
+  email: 'root@example.com.tw',
+  password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
+  role: 'admin',
+  name: 'root',
+  account: 'root',
+  created_at: new Date(),
+  updated_at: new Date()
+}]
+
+const numUsers = 5
+for (let i = 1; i <= numUsers; i++) {
+  const introLength = 160
+  const user = {
+    email: `user${i}@example.com`,
+    password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
+    role: 'user',
+    name: `user${i}`,
+    account: `user${i}`,
+    introduction: faker.lorem.text().slice(0, introLength),
+    created_at: new Date(),
+    updated_at: new Date()
+  }
+  Users.push(user)
+}
+
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', Users, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', Users, {})
+  }
+}
