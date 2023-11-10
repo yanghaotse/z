96ef9dd1da802d44232a40a760231e391a6c606b
yang haotse <ha91252@gmail.com>
2023-11-10 17:45:15 +0800

    feat: add recommend followings

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,11 +1,15 @@
 const { User, Tweet, Reply, Like, Followship } = require('../models')
+const { Sequelize, Op } = require('sequelize')
 const { getUser } = require('../helpers/auth-helpers')
-
-
+const { getRecommendedFollowings } = require('../services/user-service')
 const tweetController = {
   getTweets: async(req, res, next) => {
     try {
       const userId = getUser(req).id
+      // 查詢:當前使用者、推薦使用者、所有貼文
+      const currentUser = await User.findByPk(userId, { raw: true, nest: true })
+      const recommendFollowings = await getRecommendedFollowings(userId)
+
       const tweets = await Tweet.findAll({
         include: [
           User,
@@ -19,9 +23,8 @@ const tweetController = {
         likesCount: tweet.Likes.length,
         isLiked: tweet.Likes.some(like => like.userId === userId)
       }))
-      const currentUser = await User.findByPk(userId, { raw: true, nest: true})
 
-      res.render('tweets', { tweets: tweetsData, currentUser })
+      res.render('tweets', { tweets: tweetsData, currentUser, recommendFollowings })
     } catch(err) {
       next(err)
     }
---
+++ services/user-service.js
@@ -0,0 +1,41 @@
+const { Sequelize, Op } = require('sequelize')
+const { User } = require('../models')
+
+// 使用者推薦追蹤名單
+const getRecommendedFollowings = async(userId) => {
+  try {
+    const recommendedFollowings = await User.findAll({
+      where: {
+        role: 'user',
+        id: { [Op.not]: userId }
+      },
+      include: [
+        { model: User, as: 'Followers'}
+      ],
+      // 計算追蹤數量並降冪排序
+      attributes: {
+        include: [
+          [Sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.following_id = User.id)'), 'followersCount']
+        ]
+      },
+      order: [
+        [Sequelize.literal('followersCount'), 'DESC']
+      ],
+      limit: 10
+    })
+
+    const recommendedFollowingsData = recommendedFollowings.map(r => ({
+      ...r.toJSON(),
+      isFollowed: r.Followers.some(f=> f.id === userId)
+    }))
+
+    return recommendedFollowingsData
+  } catch(err) {
+    console.error(err)
+    throw err
+  }
+}
+
+module.exports = { 
+  getRecommendedFollowings
+}
