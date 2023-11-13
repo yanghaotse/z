3544904fe0daf8c20e4b52dc4f6da4a3af0583df
yang haotse <ha91252@gmail.com>
2023-11-13 18:39:12 +0800

    feat: add put user setting

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -261,6 +261,69 @@ const userController = {
     } catch(err) {
       next(err)
     }
+  },
+  putUserSetting: async(req, res, next) => {
+    try {
+      const currentUser = getUser(req)
+      const { account, name, email, password, passwordCheck } = req.body
+
+      if (!account || !name || !email) throw new Error ('帳戶、名稱、Email 為必填欄位')
+      if (password !== passwordCheck) throw new Error('密碼與確認密碼不相符')
+      if (name > 50) throw new Error('字數超出上限')
+
+      // 查詢是否有已存在的 account 或 email
+      const existingUserData = await User.findOne({
+        where: {
+          [Op.or]: [
+            {
+              [Op.and]: [
+                { account: account },
+                { account: { [Op.notLike]: currentUser.account }}
+              ]
+            },
+            {
+              [Op.and]: [
+                { email: email },
+                { email: { [Op.notLike]: currentUser.email }}
+              ]
+            }
+          ]
+        }
+      })
+      if (existingUserData) {
+        if (existingUserData.toJSON().account === account) throw new Error('帳號已經被使用')
+        if (existingUserData.toJSON().email === email) throw new Error('Email 已經被使用')
+      }
+      // 若使用者有修改密碼
+      if (password) {
+        const hashPassword = await hash.bcrypt(password, 10)
+        await User.update({
+          account,
+          name,
+          email,
+          password: hashPassword
+        }, {
+          where: {
+            id: currentUser.id
+          }
+        })
+      } else {
+        await User.update({
+          account,
+          name,
+          email
+        }, {
+          where: {
+            id: currentUser.id
+          }
+        })
+      }
+
+      req.flash('success_messages', '編輯成功!')
+      res.redirect(`/users/${currentUser.id}/setting`)
+    } catch(err) {
+      next(err)
+    }
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,7 @@ router.get('/users/:id/followings', authenticated, userController.getUserFollowi
 router.get('/users/:id/replies', authenticated, userController.getUserReplies)
 router.get('/users/:id/likes', authenticated, userController.getUserLikes)
 router.get('/users/:id/setting', authenticated, userController.getUserSetting)
+router.put('/users/:id/setting', authenticated, userController.putUserSetting)
 router.post('/followships', authenticated, userController.addFollowing)
 router.delete('/followships/:id', authenticated, userController.removeFollowing)
 
