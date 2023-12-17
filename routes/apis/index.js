2369969343d9f58ecc87610e745fed362e4112a1
yang haotse <ha91252@gmail.com>
2023-12-17 18:34:23 +0800

    feat: api signin add jwt token

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-
+const passport = require('../../config/passport')
 const tweetController = require('../../controllers/apis/tweet-controller')
+const userController = require('../../controllers/apis/user-controller')
 
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.get('/tweets', tweetController.getTweets)
 
 module.exports = router
