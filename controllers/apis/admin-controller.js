802a22cd5e0358a5c11a3a489eab8f6a65ef9a1a
yang haotse <ha91252@gmail.com>
2023-12-18 16:15:18 +0800

    feat: api admin signin add jwt token

---
+++ controllers/apis/admin-controller.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const { User, Tweet, Reply, Like } = require('../../models')
+const { Op } = require('sequelize')
+const { getUser } = require('../../helpers/auth-helpers')
+
+const adminController = {
+  adminSignIn: async(req, res, next) => {
+    try {
+      const userData = getUser(req).toJSON() // 因在路由處設定{session: false}沒進入返序列化程序，所以資料是sequelize物件
+      delete userData.password
+      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '90d' }) // 簽發 JWT，效期為 90 天
+      return res.json({
+        status: 'success',
+        data: {
+          token,
+          user: userData
+        }
+      })
+    } catch(err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = adminController
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 const passport = require('../../config/passport')
-const { apiErrorHandler } = require('../../middleware/error-handler')
 const tweetController = require('../../controllers/apis/tweet-controller')
 const userController = require('../../controllers/apis/user-controller')
-const { authenticated } = require('../../middleware/api-auth')
+const adminController = require('../../controllers/apis/admin-controller')
+const admin = require('./modules/admin')
+const { authenticated, authenticateAdmin } = require('../../middleware/api-auth')
+const { apiErrorHandler } = require('../../middleware/error-handler')
+
+router.post('/admin/signin', passport.authenticate('local', { session: false }), adminController.adminSignIn)
+router.use('/admin', authenticateAdmin, admin)
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.get('/tweets', authenticated, tweetController.getTweets)
